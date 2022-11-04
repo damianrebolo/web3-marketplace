e8f6223039b4e81b0dc55e1475ca277568e8c5ae
Damian Rebolo <damianrebolo@gmail.com>
2022-11-04 12:25:45 +0800

    refactor: move thirdweb provider to marketplace

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,7 @@
 import type { AppProps } from "next/app";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 import "../styles/globals.css";
 
-import { CoreNavbar } from "../components/shared/core";
-
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ThirdwebProvider
-    desiredChainId={ChainId.Goerli}
-    chainRpc={{
-      [ChainId.Goerli]: `https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
-    }}
-  >
-    <CoreNavbar />
-    <Component {...pageProps} />
-  </ThirdwebProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
 export default MyApp;
--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -1,29 +1,39 @@
 import type { NextPage } from "next";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 import { Container, GridContainer } from "../components/shared/ui";
+import { CoreNavbar } from "../components/shared/core";
 
 import { ActiveListings, MarketplaceCard } from "../components/pages/Marketplace";
 
 const MarketplacePage: NextPage = () => (
-  <Container className="py-5" fluid>
-    <ActiveListings>
-      {(listings, contract) => (
-        <GridContainer>
-          {listings?.map((listing) => (
-            <MarketplaceCard
-              key={listing?.id}
-              contract={contract}
-              image={listing?.asset?.image}
-              name={listing?.asset?.name}
-              currencyValue={listing?.buyoutCurrencyValuePerToken?.displayValue}
-              sellerAddress={listing?.sellerAddress}
-              tokenId={listing?.tokenId}
-            />
-          ))}
-        </GridContainer>
-      )}
-    </ActiveListings>
-  </Container>
+  <ThirdwebProvider
+    desiredChainId={ChainId.Goerli}
+    chainRpc={{
+      [ChainId.Goerli]: `https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
+    }}
+  >
+    <CoreNavbar />
+    <Container className="py-5" fluid>
+      <ActiveListings>
+        {(listings, contract) => (
+          <GridContainer>
+            {listings?.map((listing) => (
+              <MarketplaceCard
+                key={listing?.id}
+                contract={contract}
+                image={listing?.asset?.image}
+                name={listing?.asset?.name}
+                currencyValue={listing?.buyoutCurrencyValuePerToken?.displayValue}
+                sellerAddress={listing?.sellerAddress}
+                tokenId={listing?.tokenId}
+              />
+            ))}
+          </GridContainer>
+        )}
+      </ActiveListings>
+    </Container>
+  </ThirdwebProvider>
 );
 
 export default MarketplacePage;
