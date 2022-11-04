b731b113313a218aa9d88a347d2bde612ae3f59b
Damian Rebolo <damianrebolo@gmail.com>
2022-11-04 10:34:49 +0800

    fix: prettier error

--- src/stories/ui/Card.stories.tsx
+++ src/stories/ui/Card.stories.tsx
@@ -33,11 +33,16 @@ export default {
 const Template: ComponentStory<typeof Card> = (args) => (
   <Card {...args}>
     <Card.Image>
-      <Image src="https://picsum.photos/id/188/720/400" alt="Picture of the author" layout="fill" className="object-cover object-center" />
+      <Image
+        src="https://picsum.photos/id/188/720/400"
+        alt="Picture of the author"
+        layout="fill"
+        className="object-cover object-center"
+      />
     </Card.Image>
     <Card.Body>
-      Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar
-      de las industrias desde el año 1500, cuando un impresor
+      Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto
+      de relleno estándar de las industrias desde el año 1500, cuando un impresor
     </Card.Body>
   </Card>
 );
