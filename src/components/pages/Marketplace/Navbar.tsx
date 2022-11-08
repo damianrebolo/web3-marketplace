26cf739168d3a36ffb782187c035231b6be83fdc
Damian Rebolo <damianrebolo@gmail.com>
2022-11-08 09:41:49 +0800

    fix: navbar style

--- src/components/pages/Marketplace/Navbar.tsx
+++ src/components/pages/Marketplace/Navbar.tsx
@@ -12,12 +12,12 @@ export const MarketplaceNavbar: React.FC = () => (
     <Navbar.Brand href="/">
       <Image src="/assets/images/logo.png" alt="Web3 Portfolio" width={40} height={40} /> Web3 Portfolio
     </Navbar.Brand>
-    <Nav className="ml-8">
+    <Nav className="ml-8 hidden sm:block">
       <Nav.Link href="/nfts">NFT Collection</Nav.Link>
       <Nav.Link href="/listings">Listings</Nav.Link>
     </Nav>
-    <Navbar.Darkmode className="sm:ml-auto" />
-    <Navbar.Toggle className="ml-auto sm:ml-3">
+    <Navbar.Darkmode className="ml-auto" />
+    <Navbar.Toggle className="ml-3">
       <WalletIcon className="w-10 h-10 cursor-pointer" />
     </Navbar.Toggle>
     <Navbar.OffCanvas>
