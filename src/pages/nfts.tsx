cb4005ffab3f460214e0ad99aaf88c22de4d93a1
Damian Rebolo <damianrebolo@gmail.com>
2022-11-08 11:59:21 +0800

    feat: Table component created

--- src/pages/nfts.tsx
+++ src/pages/nfts.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
-import { Container } from "components/shared/ui";
+import { Container, Table } from "components/shared/ui";
 import { useContract, useNFTs } from "@thirdweb-dev/react";
 import { Loading, Error } from "components/shared/core";
 import { Button } from "components/shared/ui/Button";
@@ -29,49 +29,42 @@ const NftsPage: NextPage = () => {
         </Link>
       </div>
 
-      <div className="overflow-x-auto relative my-10">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="py-3 px-6">
-                Token Id
+      <Table>
+        <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="py-3 px-6">
+              Token Id
+            </th>
+            <th scope="col" className="py-3 px-6">
+              Media
+            </th>
+            <th scope="col" className="py-3 px-6">
+              Name
+            </th>
+            <th scope="col" className="py-3 px-6">
+              Description
+            </th>
+            <th scope="col" className="py-3 px-6">
+              Owner
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {nfts?.map((nft) => (
+            <tr key={nft.metadata.id} className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {nft.metadata.id}
               </th>
-              <th scope="col" className="py-3 px-6">
-                Media
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Name
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Description
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Owner
-              </th>
+              <td className="py-4 px-6 relative">
+                <Image src={nft.metadata.image as string} alt={nft.metadata.name as string} height="128" width="128" />
+              </td>
+              <td className="py-4 px-6">{nft.metadata.name}</td>
+              <td className="py-4 px-6">{nft.metadata.description}</td>
+              <td className="py-4 px-6">{cutAddress(nft.owner)}</td>
             </tr>
-          </thead>
-          <tbody>
-            {nfts?.map((nft) => (
-              <tr key={nft.metadata.id} className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  {nft.metadata.id}
-                </th>
-                <td className="py-4 px-6 relative">
-                  <Image
-                    src={nft.metadata.image as string}
-                    alt={nft.metadata.name as string}
-                    height="128"
-                    width="128"
-                  />
-                </td>
-                <td className="py-4 px-6">{nft.metadata.name}</td>
-                <td className="py-4 px-6">{nft.metadata.description}</td>
-                <td className="py-4 px-6">{cutAddress(nft.owner)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </Table>
     </Container>
   );
 };
