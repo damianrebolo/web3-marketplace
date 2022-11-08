f6412d31210f4446e64c4205ec162a83109cc790
Damian Rebolo <damianrebolo@gmail.com>
2022-11-08 12:49:43 +0800

    feat: Nfts logic moved to NftList component

--- src/pages/nfts.tsx
+++ src/pages/nfts.tsx
@@ -3,70 +3,66 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Container, Table } from "components/shared/ui";
-import { useContract, useNFTs } from "@thirdweb-dev/react";
-import { Loading, Error } from "components/shared/core";
 import { Button } from "components/shared/ui/Button";
 
 import { cutAddress } from "utils";
+import { NftList } from "components/pages/nfts";
 
-const NftsPage: NextPage = () => {
-  const { contract } = useContract(process.env.NEXT_PUBLIC_CONTRACT_NFTS);
-  const { data: nfts, isLoading, error } = useNFTs(contract, { start: 0, count: 100 });
+const NftsPage: NextPage = () => (
+  <Container className="m-10">
+    <div className="flex justify-between items-center my-5">
+      <h2 className="font-semibold text-xl text-gray-700 dark:text-white">NFT Collection</h2>
+      <Link href="/mint">
+        <Button variant="secondary">Mint + Listing</Button>
+      </Link>
+    </div>
 
-  if (isLoading) {
-    return <Loading />;
-  } else if (error) {
-    const errorParsed = JSON.parse(JSON.stringify(error));
-    return <Error>{errorParsed?.reason}</Error>;
-  }
-
-  return (
-    <Container className="m-10">
-      <div className="flex justify-between items-center my-5">
-        <h2 className="font-semibold text-xl text-gray-700 dark:text-white">NFT Collection</h2>
-        <Link href="/mint">
-          <Button variant="secondary">Mint + Listing</Button>
-        </Link>
-      </div>
-
-      <Table>
-        <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="py-3 px-6">
-              Token Id
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Media
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Name
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Description
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Owner
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {nfts?.map((nft) => (
-            <tr key={nft.metadata.id} className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {nft.metadata.id}
+    <NftList>
+      {(nfts) => (
+        <Table>
+          <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="py-3 px-6">
+                Token Id
               </th>
-              <td className="py-4 px-6 relative">
-                <Image src={nft.metadata.image as string} alt={nft.metadata.name as string} height="128" width="128" />
-              </td>
-              <td className="py-4 px-6">{nft.metadata.name}</td>
-              <td className="py-4 px-6">{nft.metadata.description}</td>
-              <td className="py-4 px-6">{cutAddress(nft.owner)}</td>
+              <th scope="col" className="py-3 px-6">
+                Media
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Name
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Description
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Owner
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Container>
-  );
-};
+          </thead>
+          <tbody>
+            {nfts?.map((nft) => (
+              <tr key={nft.metadata.id} className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {nft.metadata.id}
+                </th>
+                <td className="py-4 px-6 relative">
+                  <Image
+                    src={nft.metadata.image as string}
+                    alt={nft.metadata.name as string}
+                    height="128"
+                    width="128"
+                  />
+                </td>
+                <td className="py-4 px-6">{nft.metadata.name}</td>
+                <td className="py-4 px-6">{nft.metadata.description}</td>
+                <td className="py-4 px-6">{cutAddress(nft.owner)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </NftList>
+  </Container>
+);
 
 export default NftsPage;
