a8d96d414efe485892bd769fbc1981070aafff6f
Damian Rebolo <damianrebolo@gmail.com>
2022-11-10 11:11:08 +0700

    feat: add Head title and meta tag

--- src/components/shared/ui/GridContainer.tsx
+++ src/components/shared/ui/GridContainer.tsx
@@ -1,5 +1,5 @@
 interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
 export const GridContainer: React.FC<Props> = ({ ...props }) => (
-  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-10 py-10" {...props} />
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 my-10" {...props} />
 );
--- src/pages/listings.tsx
+++ src/pages/listings.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import Image from "next/image";
 
 import { Container, Table } from "components/shared/ui";
@@ -9,59 +10,65 @@ import { Listings } from "components/pages/Listings";
 
 const ListingsPage: NextPage = () => {
   return (
-    <Container className="m-10">
-      <h2 className="my-5 font-semibold text-xl text-gray-700 dark:text-white">Contract Listings</h2>
+    <>
+      <Head>
+        <title>Web3 Portfolio - Listings</title>
+        <meta property="og:title" content="Web3 Portfolio - Listings" key="title" />
+      </Head>
+      <Container className="m-10">
+        <h1 className="my-5 font-semibold text-xl text-gray-700 dark:text-white">Contract Listings</h1>
 
-      <Listings>
-        {(listings) => (
-          <Table>
-            <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="py-3 px-6">
-                  Listing Id
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Media
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Name
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Seller
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Price
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Type
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {listings?.map((listing) => (
-                <tr key={listing.id} className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
-                  <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {listing.id}
+        <Listings>
+          {(listings) => (
+            <Table>
+              <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="py-3 px-6">
+                    Listing Id
                   </th>
-                  <td className="py-4 px-6 relative">
-                    <Image
-                      src={listing.asset.image as string}
-                      alt={listing.asset.name as string}
-                      height="128"
-                      width="128"
-                    />
-                  </td>
-                  <td className="py-4 px-6">{listing.asset.name}</td>
-                  <td className="py-4 px-6">{cutAddress(listing.sellerAddress)}</td>
-                  <td className="py-4 px-6">{ethers.utils.formatEther(listing.buyoutPrice)}</td>
-                  <td className="py-4 px-6">{listing.type === 0 ? "Direct Listing" : "Auction Listing"}</td>
+                  <th scope="col" className="py-3 px-6">
+                    Media
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                    Name
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                    Seller
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                    Price
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                    Type
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-        )}
-      </Listings>
-    </Container>
+              </thead>
+              <tbody>
+                {listings?.map((listing) => (
+                  <tr key={listing.id} className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {listing.id}
+                    </th>
+                    <td className="py-4 px-6 relative">
+                      <Image
+                        src={listing.asset.image as string}
+                        alt={listing.asset.name as string}
+                        height="128"
+                        width="128"
+                      />
+                    </td>
+                    <td className="py-4 px-6">{listing.asset.name}</td>
+                    <td className="py-4 px-6">{cutAddress(listing.sellerAddress)}</td>
+                    <td className="py-4 px-6">{ethers.utils.formatEther(listing.buyoutPrice)}</td>
+                    <td className="py-4 px-6">{listing.type === 0 ? "Direct Listing" : "Auction Listing"}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )}
+        </Listings>
+      </Container>
+    </>
   );
 };
 
