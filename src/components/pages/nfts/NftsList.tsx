e11526b129f38cd3561b5b07b5ce04918f58dc2a
Damian Rebolo <damianrebolo@gmail.com>
2022-11-11 09:40:57 +0800

    feat: Pagination component works, then abstract functionality

--- src/components/pages/nfts/NftsList.tsx
+++ src/components/pages/nfts/NftsList.tsx
@@ -1,22 +1,26 @@
+import { ReactNode } from "react";
 import { useContract, useNFTs } from "@thirdweb-dev/react";
 import { NFT } from "@thirdweb-dev/sdk";
 
-import { Loading, Error } from "components/shared/core";
-import { ReactNode } from "react";
+import { Loading } from "components/shared/core";
+
+interface Pagination {
+  start: number;
+  count: number;
+}
 
 interface Props {
   children: (listings: NFT[]) => ReactNode;
+  pagination: Pagination;
 }
 
-export const NftList: React.FC<Props> = ({ children }) => {
+export const NftList: React.FC<Props> = ({ children, pagination }) => {
+  const { start, count } = pagination;
   const { contract } = useContract(process.env.NEXT_PUBLIC_CONTRACT_NFTS);
-  const { data: nfts = [], isLoading, error } = useNFTs(contract, { start: 0, count: 100 });
+  const { data: nfts = [], isLoading } = useNFTs(contract, { start, count });
 
   if (isLoading) {
     return <Loading />;
-  } else if (error) {
-    const errorParsed = JSON.parse(JSON.stringify(error));
-    return <Error>{errorParsed?.reason}</Error>;
   }
 
   return <>{children(nfts)}</>;
