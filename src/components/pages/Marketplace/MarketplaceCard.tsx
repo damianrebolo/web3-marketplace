92f6d4c2c6d4f7084f2238070047e60d46d227f5
Damian Rebolo <damianrebolo@gmail.com>
2022-11-11 12:15:50 +0800

    feat: marketplace as link for detail page

--- src/components/pages/Marketplace/MarketplaceCard.tsx
+++ src/components/pages/Marketplace/MarketplaceCard.tsx
@@ -7,6 +7,7 @@ import { EtherIcon, UserIcon } from "../../shared/icons";
 import Card from "../../shared/ui/Card";
 import { Button } from "../../shared/ui/Button";
 import { useToast } from "components/shared/core/Toaster/ToasterProvider";
+import Link from "next/link";
 
 interface Props {
   name: string | number | undefined;
@@ -14,9 +15,17 @@ interface Props {
   sellerAddress: string;
   id: BigNumberish;
   currencyValue: string;
+  contractAddress: string;
 }
 
-export const MarketplaceCard: React.FC<Props> = ({ name, image, sellerAddress, id, currencyValue }) => {
+export const MarketplaceCard: React.FC<Props> = ({
+  name,
+  image,
+  sellerAddress,
+  id,
+  currencyValue,
+  contractAddress,
+}) => {
   const isMismatched = useNetworkMismatch();
   const [, switchNetwork] = useNetwork();
   const address = useAddress() as string;
@@ -48,30 +57,30 @@ export const MarketplaceCard: React.FC<Props> = ({ name, image, sellerAddress, i
   };
 
   return (
-    <Card shadow="lg">
-      {image && (
-        <Card.Image>
-          <Image src={image} alt="Picture of the author" layout="fill" className="object-cover object-center" />
-        </Card.Image>
-      )}
-      <Card.Body>
-        {name && <h3 className="text-2xl font-semibold mb-3 uppercase">{name}</h3>}
+    <Link href={`/${contractAddress}/${id}`}>
+      <a>
+        <Card shadow="lg">
+          {image && (
+            <Card.Image>
+              <Image src={image} alt="Picture of the author" layout="fill" className="object-cover object-center" />
+            </Card.Image>
+          )}
+          <Card.Body>
+            <div className="flex flex-nowrap justify-between items-center mb-5">
+              <h3 className="text-2xl font-semibold uppercase">{name}</h3>
+              <div className="flex flex-nowrap items-center justify-end">
+                <EtherIcon />
+                <span className="text-md font-bold">{currencyValue}</span>
+              </div>
+            </div>
 
-        <h4 className="flex justify-start items-center flex-nowrap mb-3">
-          <UserIcon />
-          <span className="ml-2 truncate text-xs">{sellerAddress}</span>
-        </h4>
-
-        <div className="flex justify-between items-center flex-wrap ">
-          <Button variant="dark" onClick={() => onBuyNFT()}>
-            Buy Now
-          </Button>
-          <div className="flex justify-center items-center">
-            <EtherIcon />
-            <span className="text-md font-bold">{currencyValue}</span>
-          </div>
-        </div>
-      </Card.Body>
-    </Card>
+            <h4 className="flex justify-start items-center flex-nowrap mb-3">
+              <UserIcon />
+              <span className="ml-2 truncate text-xs">{sellerAddress}</span>
+            </h4>
+          </Card.Body>
+        </Card>
+      </a>
+    </Link>
   );
 };
