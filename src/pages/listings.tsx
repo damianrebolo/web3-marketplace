f3e82914a28b7c23b7d87a0d851a45ca04d3248d
Damian Rebolo <damianrebolo@gmail.com>
2022-11-15 08:14:52 +0800

    feat: new table applied

--- src/pages/listings.tsx
+++ src/pages/listings.tsx
@@ -4,12 +4,13 @@ import { useRouter } from "next/router";
 import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 
+import { ColumnDef } from "@tanstack/react-table";
 import { DirectListing } from "@thirdweb-dev/sdk";
-import { Column } from "react-table";
 
 import { cutAddress } from "utils";
-import Head from "next/head";
+
 import { Container } from "components/shared/ui";
 import { Listings } from "components/pages/Listings";
 import { Table } from "components/shared/ui/Table/";
@@ -18,44 +19,40 @@ import { Button } from "components/shared/ui/Button";
 const ListingsPage: NextPage = () => {
   const router = useRouter();
 
-  const columns: Array<Column<DirectListing>> = useMemo(
-    () => [
-      {
-        Header: () => <div className="text-center">LISTING ID</div>,
-        id: "id",
-        accessor: (row: DirectListing) => <div className="text-center">{row.id}</div>,
-      },
-      {
-        Header: "MEDIA",
-        id: "media",
-        accessor: (row: DirectListing) => (
-          <Image src={row.asset.image as string} height="128" width="128" alt={row.asset.name as string} />
-        ),
-      },
-      {
-        Header: "NAME",
-        id: "name",
-        accessor: (row: DirectListing) => row.asset.name,
-      },
-      {
-        Header: "SELLER",
-        id: "sellerAddress",
-        accessor: (row: DirectListing) => cutAddress(row.sellerAddress),
-      },
-      {
-        Header: "PRICE",
-        id: "price",
-        accessor: (row: DirectListing) => row.buyoutCurrencyValuePerToken.displayValue,
-      },
-      {
-        Header: "TYPE",
-        id: "type",
-        accessor: (row: DirectListing) => (row.type === 0 ? "Direct Listing" : "Auction Listing"),
-      },
-    ],
-
-    []
-  );
+  const ListingsColumns: ColumnDef<DirectListing>[] = [
+    {
+      header: () => <div className="text-center">LISTING ID</div>,
+      id: "id",
+      accessorFn: (row: DirectListing) => <div className="text-center">{row.id}</div>,
+      cell: (info: { getValue: () => any }) => info.getValue(),
+    },
+    {
+      header: "MEDIA",
+      id: "media",
+      accessorFn: (row: DirectListing) => (
+        <Image src={row.asset.image as string} height="90" width="90" alt={row.asset.name as string} />
+      ),
+      cell: (info: { getValue: () => any }) => info.getValue(),
+    },
+    {
+      header: "NAME",
+      accessorKey: "asset.name",
+    },
+    {
+      header: "SELLER",
+      id: "sellerAddress",
+      accessorFn: (row: DirectListing) => cutAddress(row.sellerAddress),
+    },
+    {
+      header: "PRICE",
+      accessorKey: "buyoutCurrencyValuePerToken.displayValue",
+    },
+    {
+      header: "TYPE",
+      id: "type",
+      accessorFn: (row: DirectListing) => (row.type === 0 ? "Direct Listing" : "Auction Listing"),
+    },
+  ];
 
   const onRowClicked = useCallback((obj: DirectListing) => router.push(`/${obj.assetContractAddress}/${obj.id}`), []);
 
@@ -74,9 +71,9 @@ const ListingsPage: NextPage = () => {
         </div>
         <Listings>
           {(listings: DirectListing[], isLoading) => (
-            <Table
+            <Table<DirectListing>
               data={listings}
-              columns={columns}
+              columns={ListingsColumns}
               pagination={true}
               isLoading={isLoading}
               onRowClicked={onRowClicked}
--- src/pages/nfts.tsx
+++ src/pages/nfts.tsx
@@ -2,22 +2,51 @@ import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
-import { Container, Table } from "components/shared/ui";
+import { NFT } from "@thirdweb-dev/sdk";
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Container } from "components/shared/ui";
 import { Button } from "components/shared/ui/Button";
+import { NftList } from "components/pages/nfts";
 
 import { cutAddress } from "utils";
-import { NftList } from "components/pages/nfts";
-import { memo, useState } from "react";
+import { Table } from "components/shared/ui/Table/";
 
-interface Pagination {
-  start: number;
-  count: number;
-}
+const NftsPage: NextPage = () => {
+  const router = useRouter();
 
-const NftsPage: NextPage = memo(() => {
-  const [pagination, setPagination] = useState<Pagination>({ start: 0, count: 10 });
-  console.log(pagination.start, pagination.count);
+  const nftColumns: ColumnDef<NFT>[] = [
+    {
+      header: () => <div className="text-center">TOKEN ID</div>,
+      id: "tokenId",
+      accessorFn: (row: NFT) => <div className="text-center">{row.metadata.id}</div>,
+      cell: (info: { getValue: () => any }) => info.getValue(),
+    },
+    {
+      header: "MEDIA",
+      id: "media",
+      accessorFn: (row: NFT) => (
+        <Image src={row.metadata.image as string} height="90" width="90" alt={row.metadata.name as string} />
+      ),
+      cell: (info: { getValue: () => any }) => info.getValue(),
+    },
+    {
+      header: "NAME",
+      accessorKey: "metadata.name",
+    },
+    {
+      header: "DESCRIPTION",
+      accessorKey: "metadata.description",
+    },
+    {
+      header: "OWNER",
+      id: "owner",
+      accessorFn: (row: NFT) => cutAddress(row.owner),
+    },
+  ];
+
   return (
     <>
       <Head>
@@ -31,112 +60,12 @@ const NftsPage: NextPage = memo(() => {
             <Button variant="secondary">Mint</Button>
           </Link>
         </div>
-
-        <NftList pagination={pagination}>
-          {(nfts) => (
-            <>
-              <Table>
-                <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" className="py-3 px-6">
-                      Token Id
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                      Media
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                      Name
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                      Description
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                      Owner
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {nfts?.map((nft) => (
-                    <tr key={nft.metadata.id} className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {nft.metadata.id}
-                      </th>
-                      <td className="py-4 px-6 relative">
-                        <Image
-                          src={nft.metadata.image as string}
-                          alt={nft.metadata.name as string}
-                          height="128"
-                          width="128"
-                        />
-                      </td>
-                      <td className="py-4 px-6">{nft.metadata.name}</td>
-                      <td className="py-4 px-6">{nft.metadata.description}</td>
-                      <td className="py-4 px-6">{cutAddress(nft.owner)}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-              <div className="flex flex-col items-center">
-                <span className="text-sm text-gray-700 dark:text-gray-400">
-                  Showing <span className="font-semibold text-gray-900 dark:text-white">{pagination.start + 1}</span> to{" "}
-                  <span className="font-semibold text-gray-900 dark:text-white">
-                    {pagination.start + pagination.count}
-                  </span>
-                </span>
-                <div className="inline-flex mt-2 xs:mt-0">
-                  <button
-                    disabled={pagination.start === 0}
-                    onClick={() => {
-                      setPagination({ ...pagination, start: pagination.start - pagination.count });
-                    }}
-                    className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:cursor-not-allowed"
-                  >
-                    <svg
-                      aria-hidden="true"
-                      className="mr-2 w-5 h-5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                    Prev
-                  </button>
-                  <button
-                    disabled={pagination.count > nfts.length}
-                    onClick={() => {
-                      setPagination({ ...pagination, start: pagination.start + pagination.count });
-                    }}
-                    className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-r border-0 border-l border-gray-700 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:cursor-not-allowed"
-                  >
-                    Next
-                    <svg
-                      aria-hidden="true"
-                      className="ml-2 w-5 h-5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </>
-          )}
+        <NftList>
+          {(nfts, isLoading) => <Table<NFT> data={nfts} columns={nftColumns} pagination={true} isLoading={isLoading} />}
         </NftList>
       </Container>
     </>
   );
-});
-NftsPage.displayName = "NFT Collection";
+};
 
 export default NftsPage;
