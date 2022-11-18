c126dfa43b3de677c8d1f1522ccf0fd97d4af0bd
Damian Rebolo <damianrebolo@gmail.com>
2022-11-18 10:58:54 +0800

    feat: marketplace and details page are ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import { Container } from "../components/shared/ui";
+import { Container, GridContainer } from "../components/shared/ui";
+import { MarketplaceCard } from "components/shared/core";
 
-import { ActiveListings, MarketplaceCard } from "../components/pages/Marketplace";
+import { DirectListing, ThirdwebSDK } from "@thirdweb-dev/sdk";
 
-const Home: NextPage = () => (
+interface Props {
+  listings: DirectListing[];
+}
+
+const Home: NextPage<Props> = ({ listings }) => (
   <>
     <Head>
       <title>Web3 Portfolio - Marketplace</title>
@@ -13,25 +18,32 @@ const Home: NextPage = () => (
     </Head>
     <Container className="m-10">
       <h1 className="my-5 font-semibold text-xl text-gray-700 dark:text-white">Marketplace</h1>
-      <ActiveListings>
-        {(listings) => (
-          <>
-            {listings?.map((listing) => (
-              <MarketplaceCard
-                key={listing?.id}
-                image={listing?.asset?.image}
-                name={listing?.asset?.name}
-                currencyValue={listing?.buyoutCurrencyValuePerToken?.displayValue}
-                sellerAddress={listing?.sellerAddress}
-                id={listing?.id}
-                contractAddress={listing.assetContractAddress}
-              />
-            ))}
-          </>
-        )}
-      </ActiveListings>
+      <GridContainer>
+        {listings?.map((listing) => (
+          <MarketplaceCard
+            key={listing?.id}
+            image={listing?.asset?.image}
+            name={listing?.asset?.name}
+            currencyValue={listing?.buyoutCurrencyValuePerToken?.displayValue}
+            sellerAddress={listing?.sellerAddress}
+            id={listing?.id}
+            contractAddress={listing.assetContractAddress}
+          />
+        ))}
+      </GridContainer>
     </Container>
   </>
 );
+
+export async function getServerSideProps() {
+  const sdk = new ThirdwebSDK("goerli");
+
+  const contract = await sdk.getContract(process.env.NEXT_PUBLIC_CONTRACT_MARKETPLACE as string, "marketplace");
+  const listings = (await contract.getActiveListings()) as DirectListing[];
+
+  return {
+    props: { listings: JSON.parse(JSON.stringify(listings)) }, // will be passed to the page component as props
+  };
+}
 
 export default Home;
